748c70b5da70a15d25516e8d5fc87973cc9c737d
Ben Lu <ayroblu@gmail.com>
2024-04-14 22:47:57 +0100

    wip optional and matchall

--- src/query.test.ts
+++ src/query.test.ts
@@ -1,123 +1,255 @@
 import Parser from "tree-sitter";
 import ts from "tree-sitter-typescript";
+import starlark from "tree-sitter-starlark";
 import { buildTraverseQuery } from "./query.js";
-import { traverse, traverseWithCursor } from "./traverse.js";
+import { traverseWithCursor } from "./traverse.js";
 const { tsx } = ts;
 
 describe("query", () => {
+  const parser = new Parser();
+  parser.setLanguage(tsx);
+
   it("should match valid expression", () => {
-    expect(runTest()).to.deep.equal(["expression", "callName"]);
+    const sourceCode = "let x = 1;\nconsole.log(x);";
+    const tree = parser.parse(sourceCode);
+    const query = {
+      type: "expression_statement",
+      capture: "expression",
+      items: [
+        {
+          type: "call_expression",
+          items: [
+            { field: "function", capture: "callName", text: "console.log" },
+          ],
+        },
+      ],
+    } as const;
+
+    let capture = undefined;
+    const traverseQuery = buildTraverseQuery(query, (captures) => {
+      capture = Object.keys(captures);
+    });
+    traverseWithCursor(tree.walk(), traverseQuery);
+    expect(capture).to.deep.equal(["expression", "callName"]);
   });
 
   it("should match more generalised expression", () => {
-    expect(runGeneralTest()).to.deep.equal([
-      ["identifier"],
-      ["identifier"],
-      ["identifier"],
-    ]);
-  });
-
-  it("should match each sub item", () => {
-    expect(runMultiMatchTest()).to.deep.equal([
-      { key: "key1", value: "'first'" },
-      { key: "key2", value: "'second'" },
-      { key: "key3", value: "'third'" },
-    ]);
-  });
-});
-
-const parser = new Parser();
-parser.setLanguage(tsx);
-
-function runTest() {
-  const sourceCode = "let x = 1;\nconsole.log(x);";
-  const tree = parser.parse(sourceCode);
-  const query = {
-    type: "expression_statement",
-    capture: "expression",
-    items: [
-      {
-        type: "call_expression",
-        items: [
-          { field: "function", capture: "callName", text: "console.log" },
-        ],
-      },
-    ],
-  } as const;
-
-  let capture = undefined;
-  const traverseQuery = buildTraverseQuery(query, (captures) => {
-    capture = Object.keys(captures);
-  });
-  // traverse(tree.rootNode, traverseQuery);
-  traverseWithCursor(tree.walk(), traverseQuery);
-  return capture;
-}
-
-function runGeneralTest() {
-  const sourceCode = `
+    const sourceCode = `
 const first = <Component foo="string" bar="string" />;
 const second = <Component foo="string" bar="string">{first}</Component>;
 const third = <NestedComponent foo="string" bar="string">{first}</NestedComponent>;
 const fourth = <MyComp foo="Component" bar="string">{first}</MyComp>;
   `;
-  const tree = parser.parse(sourceCode);
-  const query = {
-    type: ["jsx_opening_element", "jsx_self_closing_element"],
-    items: [
-      {
-        field: "name",
-        text: /Component/,
-      },
-      {
-        type: "jsx_attribute",
-        items: [
-          {
-            type: "property_identifier",
-            capture: "identifier",
-            text: "foo",
-          },
-        ],
-      },
-    ],
-  } as const;
+    const tree = parser.parse(sourceCode);
+    const query = {
+      type: ["jsx_opening_element", "jsx_self_closing_element"],
+      items: [
+        {
+          field: "name",
+          text: /Component/,
+        },
+        {
+          type: "jsx_attribute",
+          items: [
+            {
+              type: "property_identifier",
+              capture: "identifier",
+              text: "foo",
+            },
+          ],
+        },
+      ],
+    } as const;
 
-  const results: string[][] = [];
-  const traverseQuery = buildTraverseQuery(query, (captures) => {
-    results.push(Object.keys(captures));
+    const results: string[][] = [];
+    const traverseQuery = buildTraverseQuery(query, (captures) => {
+      results.push(Object.keys(captures));
+    });
+    traverseWithCursor(tree.walk(), traverseQuery);
+    expect(results).to.deep.equal([
+      ["identifier"],
+      ["identifier"],
+      ["identifier"],
+    ]);
   });
-  traverse(tree.rootNode, traverseQuery);
-  return results;
-}
 
-function runMultiMatchTest() {
-  const sourceCode = `
+  describe("captureAll", () => {
+    const sourceCode = `
 const a = {
   key1: 'first',
   key2: 'second',
   key3: 'third',
 }
   `.trim();
-  const tree = parser.parse(sourceCode);
-  const query = {
-    type: "object",
-    items: [
-      {
-        type: "pair",
-        items: [
-          { field: "key", capture: "key" },
-          { field: "value", capture: "value" },
-        ],
-      },
-    ],
-  } as const;
+    const tree = parser.parse(sourceCode);
 
-  const results: { key: string; value: string }[] = [];
-  const traverseQuery = buildTraverseQuery(query, (captures) => {
-    const key = captures.key.text;
-    const value = captures.value.text;
-    results.push({ key, value });
+    it("should match first item", () => {
+      const query = {
+        type: "object",
+        items: [
+          {
+            type: "pair",
+            items: [
+              { field: "key", capture: "key" },
+              { field: "value", capture: "value" },
+            ],
+          },
+        ],
+      } as const;
+
+      const results: { key: string; value: string }[] = [];
+      const traverseQuery = buildTraverseQuery(query, (captures) => {
+        const key = captures.key.text;
+        const value = captures.value.text;
+        results.push({ key, value });
+      });
+      traverseWithCursor(tree.walk(), traverseQuery);
+      expect(results).to.deep.equal([{ key: "key1", value: "'first'" }]);
+    });
+
+    it("should match each sub item", () => {
+      const query = {
+        type: "object",
+        captureAll: true,
+        items: [
+          {
+            type: "pair",
+            items: [
+              { field: "key", capture: "key" },
+              { field: "value", capture: "value" },
+            ],
+          },
+        ],
+      } as const;
+
+      const results: { key: string; value: string }[] = [];
+      const traverseQuery = buildTraverseQuery(query, (captures) => {
+        const key = captures.key.text;
+        const value = captures.value.text;
+        results.push({ key, value });
+      });
+      traverseWithCursor(tree.walk(), traverseQuery);
+      expect(results).to.deep.equal([
+        { key: "key1", value: "'first'" },
+        { key: "key2", value: "'second'" },
+        { key: "key3", value: "'third'" },
+      ]);
+    });
   });
-  traverse(tree.rootNode, traverseQuery);
-  return results;
-}
+
+  describe("optional", () => {
+    const parser = new Parser();
+    parser.setLanguage(starlark);
+    const query = {
+      type: "call",
+      items: [
+        {
+          field: "function",
+          text: "scala_library",
+        },
+        {
+          field: "arguments",
+          items: [
+            {
+              type: "keyword_argument",
+              items: [
+                {
+                  field: "name",
+                  text: "sources",
+                },
+                {
+                  field: "value",
+                  capture: "sources",
+                },
+              ],
+              optional: true,
+            },
+            {
+              type: "keyword_argument",
+              items: [
+                {
+                  field: "name",
+                  text: "dependencies",
+                },
+                {
+                  field: "value",
+                  capture: "deps",
+                },
+              ],
+            },
+            {
+              type: "keyword_argument",
+              items: [
+                {
+                  field: "name",
+                  text: "name",
+                },
+                {
+                  field: "value",
+                  capture: "target",
+                },
+              ],
+              optional: true,
+            },
+          ],
+        },
+      ],
+    } as const;
+
+    it("capture when present", () => {
+      const sourceCode = `
+scala_library(
+  name="custom_name",
+  sources=["*.scala"],
+  dependencies=[
+    "path/to/dep",
+  ],
+)
+  `.trim();
+      const tree = parser.parse(sourceCode);
+
+      const results: { target: string, sources: string[]; deps: string[] }[] = [];
+      const traverseQuery = buildTraverseQuery(query, (captures) => {
+        const target = captures.target?.text.slice(1, -1);
+        const sources = captures.sources?.namedChildren.map((n) =>
+          n.text.slice(1, -1),
+        );
+        const deps = captures.deps.namedChildren.map((n) =>
+          n.text.slice(1, -1),
+        );
+        results.push({ target, sources, deps });
+      });
+      traverseWithCursor(tree.walk(), traverseQuery);
+      expect(results).to.deep.equal([
+        { sources: ["*.scala"], deps: ["path/to/dep"], target: 'custom_name' },
+      ]);
+    });
+
+    it("omit when not present", () => {
+      const sourceCode = `
+scala_library(
+  dependencies=[
+    "path/to/dep",
+  ],
+)
+  `.trim();
+      const tree = parser.parse(sourceCode);
+
+      const results: { target: string, sources: string[]; deps: string[] }[] = [];
+      const traverseQuery = buildTraverseQuery(query, (captures) => {
+        const target = captures.target?.text.slice(1, -1);
+        const sources = captures.sources?.namedChildren.map((n) =>
+          n.text.slice(1, -1),
+        );
+        const deps = captures.deps.namedChildren.map((n) =>
+          n.text.slice(1, -1),
+        );
+        results.push({ target, sources, deps });
+      });
+      traverseWithCursor(tree.walk(), traverseQuery);
+      expect(results).to.deep.equal([
+        { deps: ["path/to/dep"], sources: undefined, target: undefined },
+      ]);
+    });
+  });
+});
--- src/query.ts
+++ src/query.ts
@@ -15,9 +15,15 @@ export function buildTraverseQuery<QueryItem extends TreeSitterQueryItem>(
         return;
       }
       const isMatch = queryItem.items.every((item) =>
-        node.namedChildren.some((child) =>
-          matchSubRecursive(child, item, captures),
-        ),
+        node.namedChildren.some((child) => {
+          const matchAll = queryItem.captureAll
+            ? {
+                remainingCounter: queryItem.items?.length ?? 1,
+                callback,
+              }
+            : undefined;
+          return matchSubRecursive(child, item, captures, matchAll);
+        }),
       );
       if (isMatch) {
         return callback(captures as any);
@@ -32,41 +38,53 @@ export function buildTraverseQuery<QueryItem extends TreeSitterQueryItem>(
   return result;
 }
 
-function matchSubRecursive(
+function checkSubQueryItem(
   node: Parser.SyntaxNode,
   subQueryItem: TreeSitterQuerySubItem,
-  captures: Record<string, Parser.SyntaxNode>,
-): boolean {
-  if (!subQueryItem.optional) {
-    if ("type" in subQueryItem) {
-      if (!toArr(subQueryItem.type).some((t) => t === node.type)) {
-        return false;
-      }
-    } else if ("field" in subQueryItem) {
-      if (
-        !toArr(subQueryItem.field).some(
-          (f) => node.parent && getField(node.parent, f) === node,
-        )
-      ) {
-        return false;
-      }
-    } else if ("wildcard" in subQueryItem) {
-    } else {
-      const exhaustiveCheck: never = subQueryItem;
-      throw new Error(`Unhandled case: ${exhaustiveCheck}`);
+) {
+  if ("type" in subQueryItem) {
+    if (!toArr(subQueryItem.type).some((t) => t === node.type)) {
+      return false;
     }
+  } else if ("field" in subQueryItem) {
     if (
-      subQueryItem.text &&
-      (typeof subQueryItem.text === "string"
-        ? node.text !== subQueryItem.text
-        : !subQueryItem.text.test(node.text))
+      !toArr(subQueryItem.field).some(
+        (f) => node.parent && getField(node.parent, f) === node,
+      )
     ) {
       return false;
     }
+  } else if ("wildcard" in subQueryItem) {
+  } else {
+    const exhaustiveCheck: never = subQueryItem;
+    throw new Error(`Unhandled case: ${exhaustiveCheck}`);
+  }
+  if (
+    subQueryItem.text &&
+    (typeof subQueryItem.text === "string"
+      ? node.text !== subQueryItem.text
+      : !subQueryItem.text.test(node.text))
+  ) {
+    return false;
+  }
+}
+type MatchAll<QueryItem extends TreeSitterQueryItem> = {
+  remainingCounter: number;
+  callback: (params: QueryCaptures<QueryItem>) => void | { skip: boolean };
+};
+function matchSubRecursive<QueryItem extends TreeSitterQueryItem>(
+  node: Parser.SyntaxNode,
+  subQueryItem: TreeSitterQuerySubItem,
+  captures: Record<string, Parser.SyntaxNode>,
+  matchAll: MatchAll<QueryItem> | undefined,
+): boolean {
+  const shouldReturn = checkSubQueryItem(node, subQueryItem);
+  if (typeof shouldReturn === "boolean") {
+    return shouldReturn;
   }
 
   if (subQueryItem.capture) {
-    if (captures[subQueryItem.capture as any]) {
+    if (!matchAll && captures[subQueryItem.capture as any]) {
       throw new Error("duplicate capture name found: " + subQueryItem.capture);
     }
     captures[subQueryItem.capture as any] = node;
@@ -74,15 +92,30 @@ function matchSubRecursive(
 
   const items = "items" in subQueryItem ? subQueryItem.items : undefined;
   if (!items) {
+    if (matchAll) {
+      matchAll.remainingCounter -= 1;
+      if (matchAll.remainingCounter === 0) {
+        matchAll.callback({ ...captures } as any);
+        return false;
+      }
+    }
     return true;
   } else {
+    if (matchAll) {
+      matchAll.remainingCounter += items.length - 1;
+    }
     return items.every((item) => {
+      let match = item.optional ?? false;
       for (const child of node.namedChildren) {
-        if (matchSubRecursive(child, item, captures)) {
-          return true;
+        if (matchSubRecursive(child, item, captures, matchAll)) {
+          if (matchAll) {
+            match = true;
+          } else {
+            return true;
+          }
         }
       }
-      return false;
+      return match;
     });
   }
 }
@@ -123,6 +156,7 @@ export type TreeSitterQueryItem<Capture extends string = string> = {
   type: string | ReadonlyArray<string>;
   capture?: Capture;
   items?: ReadonlyArray<TreeSitterQuerySubItem<Capture>>;
+  captureAll?: boolean;
 };
 type QueryCapturesName<T extends TreeSitterQueryItem> =
   T extends TreeSitterQueryItem<infer Captures> ? Captures : never;
